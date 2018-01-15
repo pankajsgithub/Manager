308970764bdafd809216b5524d81ea7fe7619c75
unknown <you@example.com>
2018-01-15 18:42:54 +0530

    UpdatedProj

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,8 +1,20 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { EMPLOYEE_UPDATE } from './types';
+
 
 export const employeeUpdate = ({ prop, value }) => {
     return {
         type: EMPLOYEE_UPDATE,
         payload: { prop, value }
+    };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+    const { currentUser } = firebase.auth();
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}`)
+            .push({ name, phone, shift })
+            .then(() => Actions.employeeList);
     };
 };
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions/EmployeeActions';
+import { employeeUpdate, employeeCreate } from '../actions/EmployeeActions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+    }
         render() {
             return (
                 <Card>
@@ -20,19 +25,32 @@ class EmployeeCreate extends Component {
                         <Input
                             label="Phone"
                             placeholder="+91 784524612"
-                            value={this.props.name}
+                            value={this.props.phone}
                             onChangeText={text => 
                                 this.props.employeeUpdate({ prop: 'phone', value: text })}
                         />
                     </CardSection>
-                    <CardSection>
-                        <Input
-                            label="Shift"
-                            placeholder="..."
-                        />
+                    <CardSection style={{ flexDirection: 'column' }}>
+                        <Text style={styles.pickerTextStyle}>
+                                Shift
+                        </Text>
+                        <Picker 
+                        style={{ flex: 1, padding: 15, paddingLeft: 20 }}
+                         selectedValue={this.props.shift}
+                         onValueChange={day => 
+                            this.props.employeeUpdate({ prop: 'shift', value: day })}
+                        >
+                            <Picker.Item label="Monday" value="Monday" />
+                            <Picker.Item label="Tuesday" value="Tuesday" />
+                            <Picker.Item label="Wednesday" value="Wednesday" />
+                            <Picker.Item label="Thursday" value="Thursday" />
+                            <Picker.Item label="Friday" value="Friday" />
+                            <Picker.Item label="Saturday" value="Saturday" />
+                            <Picker.Item label="Sunday" value="Sunday" />
+                        </Picker>
                     </CardSection>
                     <CardSection>
-                        <Button>
+                        <Button onPress={this.onButtonPress.bind(this)}>
                             Create
                     </Button>
                     </CardSection>
@@ -47,4 +65,11 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+const styles = {
+    pickerTextStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+    }
+};
+
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
